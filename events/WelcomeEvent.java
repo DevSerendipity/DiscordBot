4db02ddf5be97f06ca186aa15030095ac03c7d39
Islam Sakrak <islamsakrak2002@gmail.com>
2020-08-07 13:39:55 +0200

    Updated Welcome.java, and removed unnecessary lines

--- events/WelcomeEvent.java
+++ events/WelcomeEvent.java
@@ -6,17 +6,14 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class WelcomeEvent extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+
         String[] messageSent = event.getMessage().getContentRaw().split(" ");
-        for (String s : messageSent) {
-            System.out.println(s);
-        }
 
         if (messageSent[0].equalsIgnoreCase( Bot.prefix + "welcome") && messageSent.length == 1) {
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage("Welcome from me to!! :smile: ").queue();
-        }else if(messageSent[0].equalsIgnoreCase(Bot.prefix + "welcome") && !(messageSent[1].isEmpty()) && messageSent.length == 2){
+        } else if(messageSent[0].equalsIgnoreCase(Bot.prefix + "welcome") && !(messageSent[1].isEmpty()) && messageSent.length == 2){
             event.getChannel().sendMessage("Heyy Welcome! It's nice to have you here " + event.getMessage().getMentionedMembers().get(0).getAsMention()).queue();
-
         }
     }
 }
