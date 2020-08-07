09a42e96980d2b278154f80c246833699a1e71b2
Islam Sakrak <islamsakrak2002@gmail.com>
2020-08-07 12:57:13 +0200

    removed commends and changed the design a bit

--- BotCommandCenter.java
+++ BotCommandCenter.java
@@ -1,6 +1,7 @@
 package DiscordBot;
 
-import bot.events.WelcomeEvent;
+import DiscordBot.events.WelcomeEvent;
+
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -9,11 +10,13 @@ import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
 
 public class BotCommandCenter {
+
     public static String prefix = "!";
     public static JDA jda;
 
     public static void main(String[] args) throws LoginException {
-        jda = new JDABuilder(AccountType.BOT).setToken("NzM3NjA0Njg0MTAxMDU4NjYw.Xx_x5A.npiNIVa_UbPwdt7Tpw8tmRcVYBE").build();
+        
+        jda = new JDABuilder(AccountType.BOT).setToken("NzM3NjA0Njg0MTAxMDU4NjYw.Xx_x5A.XNOgS5gflbYtnY-NBqR4AgzPI-4").build();
 
         jda.addEventListener(new WelcomeEvent());
 
