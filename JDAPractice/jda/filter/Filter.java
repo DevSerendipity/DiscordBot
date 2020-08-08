78e449d7f667337bf6743b77a7c0273ed4f9b198
Islam Sakrak <islamsakrak2002@gmail.com>
2020-08-08 10:53:39 +0200

    Classes for filtering messages that should be not allowed to the server, it can be turned off and turned on, with reply and without it

---
+++ JDAPractice/jda/filter/Filter.java
@@ -0,0 +1,35 @@
+package DiscordBot.filter;
+
+
+import bot.filters.FilterMessage;
+import bot.filters.FilterOnOff;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class Filter extends ListenerAdapter {
+
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent e) {
+        if (FilterOnOff.filterOn) {
+            String[] LIST_OF_BAD_WORDS = {"***PROVIDE STRING BAD WORDS YOU WON T ALLOW TO THE SERVER***"};
+            String[] message = e.getMessage().getContentRaw().split(" ");
+            for (String s : message) {
+                boolean badWord = false;
+                //Check each message for each bad word
+                for (String list_of_bad_word : LIST_OF_BAD_WORDS) {
+                    if (s.equalsIgnoreCase(list_of_bad_word)) {
+                        e.getMessage().delete().queue();
+                        badWord = true;
+                        if (FilterMessage.allowed) { //Prints a message saying watch your language IF enabled by !filtermessage
+                            e.getChannel().sendMessage("Watching your mouth, swearing will not help " + Objects.requireNonNull(e.getMember()).getUser().getName() + " **Alright I am watching you** ").queue();
+                        }
+                    }
+                }
+                System.out.println(s + " " + badWord); //Reports everything to the console
+            }
+        }
+    }
+}
---
+++ JDAPractice/jda/filter/FilterOnOff.java
@@ -0,0 +1,21 @@
+package DiscordBot.filter;
+
+import DiscordBot.BotCommandCenter;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class FilterOnOff extends ListenerAdapter {
+
+    public static boolean filterOn = true;
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().equalsIgnoreCase(BotCommandCenter.prefix + "togglefilter") && filterOn) {
+            filterOn = false;
+            event.getChannel().sendMessage("The Curse-Filter has been disabled by " + event.getMember().getUser().getName()).queue();
+        } else if (event.getMessage().getContentRaw().equalsIgnoreCase(BotCommandCenter.prefix + "togglefilter") && !filterOn) {
+            filterOn = true;
+            event.getChannel().sendMessage("The Curse-Filter has been enabled by " + event.getMember().getUser().getName()).queue();
+        }
+    }
+}
