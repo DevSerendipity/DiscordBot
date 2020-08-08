b8707e5573a94f2258d2348ff0d05bf5d26bf226
Islam Sakrak <islamsakrak2002@gmail.com>
2020-08-08 10:15:17 +0200

    Adding reaction command that can bring some fun to the server

---
+++ events/Reactions.java
@@ -0,0 +1,38 @@
+package DiscordBot.events;
+
+import bot.Bot;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+
+public class Reactions extends ListenerAdapter {
+
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+
+        String[] messageSent = event.getMessage().getContentRaw().split(" ");
+
+        if (messageSent[0].equalsIgnoreCase(Bot.prefix + "react")) {
+            List<String> islam = event.getGuild().getEmotes().stream().map(e -> e.getName() + ":" + e.getId()).collect(Collectors.toList());
+            event.getMessage().addReaction(islam.get(generateRandom(islam.size()))).queue();
+            event.getMessage().addReaction(islam.get(generateRandom(islam.size()))).queue();
+        } else if (messageSent[0].equalsIgnoreCase(Bot.prefix + "reactions")) {
+            List<String> islam = event.getGuild().getEmotes().stream().map(e -> e.getName() + ":" + e.getId()).collect(Collectors.toList());
+            for (String s : islam) {
+                event.getMessage().addReaction(s).queue();
+            }
+        }
+    }
+
+    private int generateRandom(int length) {
+        Random random = new Random();
+        return random.nextInt(length);
+    }
+}
+
